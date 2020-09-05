24e5240f637d265699a6d19c4bfd1e4ba16837ca
MohammadReza Khatibi <mohammadrezakhatibi@outlook.com>
2020-09-05 13:56:33 +0430

    Initial Commit

---
+++ CITest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CITest
+//
+//  Created by MohammadReza on 9/5/20.
+//  Copyright © 2020 MohammadReza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
