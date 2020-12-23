46e83ab1e066e14efc292ccda3194b0669f42cd9
MohammadReza Khatibi <mohammadrezakhatibi@outlook.com>
2020-12-23 22:08:58 +0330

    update archiver

--- archiver.sh
+++ archiver.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
 # 1. Test project
-echo '🌘 Test project begin...'
 #xcodebuild -project 'Top Change.xcodeproj' -scheme 'Top Change' -destination 'platform=iOS Simulator,name=iPhone 8' -configuration Release test | xcpretty
 # 2. Archive project
-echo '🌗 Archive project begin...'
-xcodebuild -project 'CITest.xcodeproj' -scheme 'CITest' -destination 'generic/platform=iOS'  -allowProvisioningUpdates -archivePath 'Archive/CITest.xcarchive' -configuration Release PROVISIONING_PROFILE='iOS Team Provisioning Profile: *' clean archive | xcpretty
-# 3. Export project .ipa file
-echo '🌕 Export project  begin...'
-xcodebuild -exportArchive -archivePath 'Archive/CITest.xcarchive' -exportPath 'Release' -allowProvisioningUpdates -exportOptionsPlist 'ExportOptions.plist'
+echo '🌗 begin...'
+xcodebuild clean -project 'CITest.xcodeproj' -scheme 'CITest'
+xcodebuild -project 'CITest.xcodeproj' -scheme 'CITest' -destination 'platform=iOS Simulator,name=iPhone 8' test
+xcodebuild -project 'CITest.xcodeproj' -scheme 'CITest' -destination 'generic/platform=iOS' -configuration Release build CODE_SIGNING_ALLOWED=NO
 
 
-rm -rf 'Archive'
